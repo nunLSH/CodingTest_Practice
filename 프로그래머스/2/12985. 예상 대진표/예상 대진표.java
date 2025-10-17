608574afcc5e46d240376adb42e68b437c108cb3
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-10-17 23:04:49 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -2,31 +2,12 @@ class Solution
 {
     public int solution(int n, int a, int b)
     {
-        int answer = 0;
-        while(a > 0 && b > 0){
-            if (Math.abs(a-b) == 1){
-                if (a > b){
-                    if (a % 2 == 0 && b % 2 == 1){
-                        answer++;
-                        break;
-                    }
-                } else {
-                    if (b % 2 == 0 && a % 2 == 1){
-                        answer++;
-                        break;
-                    }
-                }
-            }
-            
-            if (a % 2 == 0) a /= 2;
-            else a = (a + 1) / 2;
-            
-            if (b % 2 == 0) b /= 2;
-            else b = (b + 1) / 2;
-                
-            answer++;
+        int ans = 0;
+        while (a != b) {
+            a = (a + 1) / 2;
+            b = (b + 1) / 2;
+            ans++;
         }
-        
-        return answer;
+        return ans;
     }
 }
