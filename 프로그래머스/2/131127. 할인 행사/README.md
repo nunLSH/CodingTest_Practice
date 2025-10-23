f270f44d079421745003b46ccc96a5c453152975
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-10-24 00:46:21 +0900

    [level 2] Title: 할인 행사, Time: 69.93 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/README.md
+++ 프로그래머스/2/131127. 할인 행사/README.md
@@ -1,10 +1,10 @@
 # [level 2] 할인 행사 - 131127 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127) 
 
 ### 성능 요약
 
-메모리: 134 MB, 시간: 89.59 ms
+메모리: 125 MB, 시간: 69.93 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 24일 00:36:29
+2025년 10월 24일 00:46:19
 
 ### 문제 설명
 
--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,6 +1,26 @@
+import java.util.*;
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
+
         int answer = 0;
+        for (int i = 0; i <= discount.length - 10; i++){ // 시작 인덱스
+            HashMap<String, Integer> map = new HashMap<>();
+            for (int j = i; j < i + 10; j++){ // 10일 연속
+                map.put(discount[j], map.getOrDefault(discount[j], 0) + 1);
+            }
+            
+            int cnt = 0;
+            for (int k = 0; k < want.length; k++){
+                if (map.containsKey(want[k])){
+                    if (map.get(want[k]) == number[k])
+                        cnt++;
+                }
+            }
+            
+            if (cnt == want.length)
+                answer++;
+        }
+        
         return answer;
     }
 }
