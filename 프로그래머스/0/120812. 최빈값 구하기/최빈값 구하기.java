a4010e6ad955bedef69249e1783c2761c7c862df
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-10-31 12:45:26 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.38 ms, Memory: 88.4 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -1,35 +1,23 @@
 import java.util.*;
 class Solution {
-    public final int MAX = 1000;
     public int solution(int[] array) {
-        ArrayList<Integer> cnt = new ArrayList<>();
-        
-        int[] arr = new int[MAX];
-        for (int i = 0; i < array.length; i++)
-            arr[array[i]]++;
-        
-        int maxCnt = arr[0];
+        int maxCnt = 0;
         int answer = 0;
-        for (int i = 0; i < MAX; i++){
-            if (arr[i] == 0)
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : array){
+            int cnt = map.getOrDefault(num, 0) + 1;
+            map.put(num, cnt);
+            
+            if (maxCnt < cnt){
+                maxCnt = cnt;
+                answer = num;
+            } else if (maxCnt == cnt)
+                answer = -1;
+            else
                 continue;
-            else {
-                if (maxCnt > arr[i])
-                    continue;
-                else if (maxCnt == arr[i])
-                    cnt.add(maxCnt);
-                else {
-                    maxCnt = arr[i];
-                    answer = i;
-                    cnt.clear();
-                    cnt.add(maxCnt);
-                }
-            }
         }
         
-        if (cnt.size() == 1)
-            return answer;
-        else
-            return -1;
+        return answer;
+        
     }
 }
