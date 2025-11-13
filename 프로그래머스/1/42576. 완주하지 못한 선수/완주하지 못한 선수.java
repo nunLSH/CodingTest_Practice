8a452c19fb51df2ecadd1e84e17ed4c64832814f
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-11-13 11:20:13 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 77.07 ms, Memory: 97.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -8,12 +8,12 @@ class Solution {
             map.put(i, map.getOrDefault(i, 0) + 1);
         
         for (String s : participant){
-            if (!map.containsKey(s) || map.getOrDefault(s, 0) == 0){
+            if (!map.containsKey(s) || map.get(s) == 0){
                 answer = s;
                 break;
             }
             
-            map.put(s, map.getOrDefault(s, 0) - 1);
+            map.put(s, map.get(s) - 1);
         }
         
         return answer;
