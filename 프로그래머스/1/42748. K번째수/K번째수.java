d147257abd83f4ef8aa14799c273c070f4fbe647
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-11-22 22:34:36 +0900

    [level 1] Title: K번째수, Time: 0.39 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -4,14 +4,11 @@ class Solution {
         int[] answer = new int[commands.length];
         
         for (int i = 0; i < commands.length; i++){
-            int len = commands[i][1] - commands[i][0] + 1;
-            int[] arr = new int[len];
+            int[] sliced 
+                = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
             
-            for (int j = 0; j < len; j++)
-                arr[j] = array[commands[i][0] - 1 + j];
-            
-            Arrays.sort(arr);
-            answer[i] = arr[commands[i][2] - 1];
+            Arrays.sort(sliced);
+            answer[i] = sliced[commands[i][2] - 1];
         }
         
         return answer;
