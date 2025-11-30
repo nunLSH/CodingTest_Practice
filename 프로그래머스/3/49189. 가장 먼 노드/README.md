f7bb1b4b1555115b1bde32f6da08862705ea7685
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-11-30 21:35:56 +0900

    [level 3] Title: 가장 먼 노드, Time: 33.13 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/README.md
+++ 프로그래머스/3/49189. 가장 먼 노드/README.md
@@ -1,10 +1,10 @@
 # [level 3] 가장 먼 노드 - 49189 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49189?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49189) 
 
 ### 성능 요약
 
-메모리: 114 MB, 시간: 21.64 ms
+메모리: 114 MB, 시간: 33.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 29일 16:20:59
+2025년 11월 30일 21:35:53
 
 ### 문제 설명
 
--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,53 +1,42 @@
 import java.util.*;
-class Node {
-    int vertex;
-    Node next;
-
-    Node(int vertex, Node next) {
-        this.vertex = vertex;
-        this.next = next;
-    }
-}
-
 class Solution {
     public int solution(int n, int[][] edge) {
-        int answer = 0;
-        Node[] adj = new Node[n+1];
-        Queue<Integer> queue = new ArrayDeque<>();
-        boolean[] visited = new boolean[n+1];
-        int[] distance = new int[n+1];
+        List<Integer>[] graph = new ArrayList[n+1];
+        for (int i = 1; i <= n; i++)
+            graph[i] = new ArrayList<>();
         
         for (int[] e : edge){
             int u = e[0];
             int v = e[1];
             
-            // 양방향
-            adj[u] = new Node(v, adj[u]);
-            adj[v] = new Node(u, adj[v]);
+            graph[u].add(v);
+            graph[v].add(u);   
         }
+        
+        int answer = 0;
+        Queue<Integer> queue = new ArrayDeque<>();
+        boolean[] visited = new boolean[n+1];
+        int[] dist = new int[n+1];
         
         visited[1] = true;
         queue.offer(1);
-        int dis = 0;
+        
         while(!queue.isEmpty()){
             int cur = queue.poll();
-            Node next = adj[cur];
             
-            while (next != null){
-                if (visited[next.vertex] == false){
-                    visited[next.vertex] = true;
-                    distance[next.vertex] = distance[cur] + 1;
-                    queue.offer(next.vertex);
+            for (int nxt : graph[cur]) {
+                if (!visited[nxt]) {
+                    visited[nxt] = true;
+                    dist[nxt] = dist[cur] + 1;
+                    queue.offer(nxt);
                 }
-                
-                next = next.next;
             }
         }
         
         // 배열에서 최댓값 구하기
-        int maxDistance = Arrays.stream(distance).max().getAsInt();
+        int maxDistance = Arrays.stream(dist).max().getAsInt();
         
-        for (int i : distance){
+        for (int i : dist){
             if (i == maxDistance)
                 answer++;
         }
