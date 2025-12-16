0f491ef93b453bfa7475f404ed8af3ca5fbcf960
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-12-17 01:00:32 +0900

    [level 2] Title: 피로도, Time: 3.39 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,50 +1,26 @@
-import java.util.*;
 class Solution {
-    public int answer = -1;
-    public void dfs(int energy, int cnt, 
-                    Stack<Integer> stack, boolean[] visited, int[][] dungeons){
-        while (!stack.isEmpty()){
-                stack.pop();
-                
-            for (int j = 0; j < dungeons.length; j++){
-                if (energy >= dungeons[j][0]){
-                    if (visited[j] == false){
-                        energy -= dungeons[j][1];
-                        stack.push(j);
-                        visited[j] = true;
-                        
-                        dfs(energy, cnt + 1, stack, visited, dungeons);
+    int answer = 0;
 
-                        visited[j] = false;
-                        energy += dungeons[j][1];
-                    } else
-                        continue;
-                } else{
-                    answer = Math.max(cnt, answer);
-                }
-            }
-        }
-        
-        return; 
-    }
-    public int solution(int k, int[][] dungeons) {   
-    
-        for (int i = 0; i < dungeons.length; i++){ // 시작점
-            int energy = k;
-            int cnt = 0;
-            Stack<Integer> stack = new Stack<>();
-            boolean[] visited = new boolean[dungeons.length];   
-            
-            if (energy >= dungeons[i][0]){
-                energy -= dungeons[i][1];
+    void dfs(int energy, int cnt, boolean[] visited, int[][] dungeons) {
+        answer = Math.max(answer, cnt);
+
+        for (int i = 0; i < dungeons.length; i++) {
+            if (!visited[i] && energy >= dungeons[i][0]) {
                 visited[i] = true;
-                stack.push(i);
-                cnt++;
-                
-                dfs(energy, cnt, stack, visited, dungeons);
+                dfs(
+                    energy - dungeons[i][1],
+                    cnt + 1,
+                    visited,
+                    dungeons
+                );
+                visited[i] = false; // 백트래킹
             }
         }
+    }
 
+    public int solution(int k, int[][] dungeons) {
+        boolean[] visited = new boolean[dungeons.length];
+        dfs(k, 0, visited, dungeons);
         return answer;
     }
 }
