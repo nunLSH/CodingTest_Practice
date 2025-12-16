daec26af16fd01c326c2188e9532cedc1488e2bb
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-12-17 00:42:11 +0900

    [level 3] Title: 단어 변환, Time: 0.59 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,48 +1,56 @@
 import java.util.*;
+
 class Solution {
-    public int cntDifference(String s1, String s2){
+
+    static class Node {
+        String word;
+        int depth;
+
+        Node(String word, int depth) {
+            this.word = word;
+            this.depth = depth;
+        }
+    }
+
+    public int cntDifference(String s1, String s2) {
         int diff = 0;
-        for (int i = 0; i < s1.length(); i++){
+        for (int i = 0; i < s1.length(); i++) {
             if (s1.charAt(i) != s2.charAt(i)) diff++;
         }
-        
         return diff;
     }
-    
-    public int solution(String begin, String target, String[] words) {
-        
-        Set<String> set = new HashSet<>();
-        for (String s : words)
-            set.add(s);
-        
-        if (!set.contains(target))
-            return 0;
-        
-        // BFS
-        Queue<String[]> q = new ArrayDeque<>();
-        boolean[] visited = new boolean[words.length];
 
-        q.offer(new String[]{begin, String.valueOf(0)});
-        
-        int answer = 0;
-        while (!q.isEmpty()){
-            String[] str = q.poll();
-            String cur = str[0];
-            int cnt = Integer.parseInt(str[1]);
-            
-            if (cur.equals(target)){
-                answer = cnt;
+    public int solution(String begin, String target, String[] words) {
+
+        boolean exists = false;
+        for (String w : words) {
+            if (w.equals(target)) {
+                exists = true;
                 break;
             }
-            
-            for (int i = 0; i < words.length; i++){
-                if (visited[i] == false && cntDifference(cur, words[i]) == 1){
+        }
+        if (!exists) return 0;
+
+        Queue<Node> q = new ArrayDeque<>();
+        boolean[] visited = new boolean[words.length];
+
+        q.offer(new Node(begin, 0));
+
+        while (!q.isEmpty()) {
+            Node cur = q.poll();
+
+            if (cur.word.equals(target)) {
+                return cur.depth;
+            }
+
+            for (int i = 0; i < words.length; i++) {
+                if (!visited[i] && cntDifference(cur.word, words[i]) == 1) {
                     visited[i] = true;
-                    q.offer(new String[]{words[i], String.valueOf(cnt + 1)});
+                    q.offer(new Node(words[i], cur.depth + 1));
                 }
             }
         }
-        
-        return answer;
+
+        return 0;
     }
 }
