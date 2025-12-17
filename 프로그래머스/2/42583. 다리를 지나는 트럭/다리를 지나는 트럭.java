f812a1ba6d8de203493d36959186559bb45abb88
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-12-17 12:15:18 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 29.72 ms, Memory: 89.9 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -10,11 +10,8 @@ class Solution {
         int sum = truck_weights[0]; // 현재 다리 위의 무게
         int cnt = 0; // 건넌 트럭의 개수
         
-        while (!bridge.isEmpty()){
+        while (cnt != len){
             int cur = bridge.peek();
-            
-            if (cnt == len)
-                break;
             
             if (bridge.size() == bridge_length){
                 int removed = bridge.poll();
