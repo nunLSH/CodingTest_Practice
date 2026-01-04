85f9c21e54a1356daa6abba7a5a68c915736a25d
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2026-01-04 22:49:38 +0900

    [level 2] Title: [1차] 캐시, Time: 36.12 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -1,68 +1,37 @@
 import java.util.*;
-class Solution {
-    
-    class Node {
-        String key;
-        Node prev, next;
-    }
-    
-    Node head, tail;
-    Map<String, Node> map = new HashMap<>();
 
+class Solution {
     public int solution(int cacheSize, String[] cities) {
         int answer = 0;
-        
-        if (cacheSize == 0) 
-            return cities.length * 5;
-        
-        // 더미 노드 생성
-        head = new Node();
-        tail = new Node();
-        head.next = tail;
-        tail.prev = head;
-        
-        for (String city : cities){
-            city = city.toLowerCase();
-            
-            // 캐시 미스
-            if (!map.containsKey(city)){
-                answer += 5;
-                
-                // 캐시가 꽉 찼으면 LRU 제거
-                if (map.size() == cacheSize) {
-                    Node lru = head.next;
-                    remove(lru);
-                    map.remove(lru.key);
-                }
-                
-                // 새 노드 추가
-                Node node = new Node();
-                node.key = city;
-                addToTail(node);
-                map.put(city, node);
+        List<String> cache = new ArrayList<>();
 
-            } 
+        // 캐시 크기가 0이면 전부 miss
+        if (cacheSize == 0) {
+            return cities.length * 5;
+        }
+
+        for (String city : cities) {
+            city = city.toLowerCase();
+
             // 캐시 히트
-            else {
+            if (cache.contains(city)) {
                 answer += 1;
-                Node node = map.get(city);
-                remove(node);
-                addToTail(node);                
+                cache.remove(city); // 기존 위치 제거
+                cache.add(city);    // 가장 최근으로 이동
+            }
+            // 캐시 미스
+            else {
+                answer += 5;
+
+                // 캐시가 꽉 찼으면 LRU 제거
+                if (cache.size() == cacheSize) {
+                    cache.remove(0);
+                }
+
+                cache.add(city);
             }
         }
-        
+
         return answer;
-    }
-    
-    void remove(Node node){
-        node.prev.next = node.next;
-        node.next.prev = node.prev;
-    }
-    
-    void addToTail(Node node){
-        node.prev = tail.prev;
-        node.next = tail;
-        tail.prev.next = node;
-        tail.prev = node;
     }
 }
