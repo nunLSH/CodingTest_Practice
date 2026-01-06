0458f34dc02980264a4bcd20cb36334fc249c891
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2026-01-06 23:51:33 +0900

    [level 2] Title: 튜플, Time: 217.45 ms, Memory: 158 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -1,32 +1,26 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(String s) {
-        
-        // 각 부분집합 분리
+
         String inner = s.substring(2, s.length() - 2);
         String[] sets = inner.split("\\},\\{");
-        
-        // 원소 개수가 적은 순
-        Arrays.sort(sets, (a, b) -> {
-            return a.split(",").length - b.split(",").length;
-        });
 
-        // 길이가 증가될 수록 이전에 없던 숫자 추가
+        Arrays.sort(sets, Comparator.comparingInt(a -> a.split(",").length));
+
         int[] answer = new int[sets.length];
         int idx = 0;
-        HashSet<String> set = new HashSet<>();
-        for (String ss : sets){
-            String[] parts = ss.split(",");
-            
-            for (String p : parts){
-                if (!set.contains(p)){
-                    answer[idx++] = Integer.parseInt(p);
-                    set.add(p);
+        HashSet<Integer> seen = new HashSet<>();
+
+        for (String ss : sets) {
+            for (String p : ss.split(",")) {
+                int num = Integer.parseInt(p);
+                if (seen.add(num)) { 
+                    answer[idx++] = num;
+                    break;
                 }
             }
-
         }
-        
         return answer;
     }
 }
