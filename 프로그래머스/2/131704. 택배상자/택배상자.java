49039103b848ae36e28c1f6a61f9838372f1ee77
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2026-01-22 01:50:10 +0900

    [level 2] Title: 택배상자, Time: 56.44 ms, Memory: 148 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -1,38 +1,35 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[] order) {
-        // 보조 컨테이너 >> 스택 (LIFO)
-        Stack<Integer> secondContainer = new Stack<>();
-        
-        // 보조 컨테이너를 활용해도 불가하면 더 이상 싣지 않음
-        int firstBox = order[0];
+        Stack<Integer> stack = new Stack<>();
         int box = 1;
         int answer = 0;
-        
-        // 첫번째 상자 전까지 보조 컨테이너에 적재
-        while (firstBox > box) {
-                secondContainer.push(box++);
-        }
-        
-        for (int i = 0; i < order.length; i++){
-            // 더 큰 경우
-            if (box < order[i]){
-                while(box != order[i])
-                    secondContainer.push(box++);
-            } else if (box > order[i]){
-                if (order[i] == secondContainer.pop())
+
+        for (int target : order) {
+            while (true) {
+                // 메인 컨테이너에서 바로 실을 수 있는 경우
+                if (box == target) {
+                    box++;
                     answer++;
-                else
                     break;
-            }
-            
-            // 컨테이너에서 내리는 박스랑 적재할 박스 동일
-            if (box == order[i]){
-                answer++;
-                box++;
+                }
+                // 보조 컨테이너에서 실을 수 있는 경우
+                else if (!stack.isEmpty() && stack.peek() == target) {
+                    stack.pop();
+                    answer++;
+                    break;
+                }
+                // 아직 메인에서 밀어 넣을 수 있는 경우
+                else if (box <= order.length) {
+                    stack.push(box++);
+                }
+                // 더 이상 방법 없음
+                else {
+                    return answer;
+                }
             }
         }
-        
         return answer;
     }
 }
