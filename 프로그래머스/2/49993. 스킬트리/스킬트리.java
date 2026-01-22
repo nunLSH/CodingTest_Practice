d5a9b501124676c13a56e332956a0e5d9f43a5f8
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2026-01-22 23:51:04 +0900

    [level 2] Title: 스킬트리, Time: 0.11 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -3,35 +3,19 @@ import java.util.*;
 class Solution {
     public int solution(String skill, String[] skill_trees) {
         int answer = 0;
-            
-        // skill에 속한 스킬이라면 sb에 append 후 비교
-        for (String skillTree : skill_trees){
-            // 큐에 스킬 순서 넣기
-            Queue<Character> q = new ArrayDeque<>();
-            for (int i = 0; i < skill.length(); i++)
-                q.offer(skill.charAt(i));
-            StringBuffer sb = new StringBuffer();
-            for (int i = 0; i < skillTree.length(); i++){
-                char s = skillTree.charAt(i);
-                if (skill.contains(Character.toString(s)))
-                    sb.append(s);
+
+        for (String tree : skill_trees) {
+            StringBuilder sb = new StringBuilder();
+
+            for (char c : tree.toCharArray()) {
+                // tree 중에 skill에 속한 글자는 sb에 append
+                if (skill.indexOf(c) != -1) sb.append(c);
             }
             
-            int cnt = 0;
-            // 큐에서 꺼내면서 순서 비교
-            for (int j = 0; j < sb.length(); j++){
-                System.out.println(sb);
-                if (!q.isEmpty() && sb.charAt(j) == q.peek()){
-                    q.poll();
-                    cnt++;
-                } else
-                    break;
-            }
-            
-            if (cnt == sb.length())
-                answer++;
+            // skill이 sb로 시작하면 answer 증가
+            if (skill.startsWith(sb.toString())) answer++;
         }
-        
+
         return answer;
     }
 }
