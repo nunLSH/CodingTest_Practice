6bfb8e72a7e592555b4333508b4c5dd0271f2ef5
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2026-01-27 23:57:29 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.02 ms, Memory: 70.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Solution {
+    public int solution(int storey) {
+        int answer = 0;
+        
+        while(storey > 0){
+            int cur = storey % 10;
+            int next = (storey / 10) % 10;
+            
+            if (cur > 5){
+                answer += (10 - cur);
+                storey = storey / 10 + 1;
+            } else if (cur < 5) {
+                answer += cur;
+                storey /= 10;
+            } else {
+                if (next >= 5) {
+                    answer += 5;
+                    storey = storey / 10 + 1;
+                } else {
+                    answer += 5;
+                    storey /= 10;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
