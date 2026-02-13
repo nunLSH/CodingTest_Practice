ce884b408002f7a710a769eea28d46d41fd7374d
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2026-02-13 14:12:05 +0900

    [level 2] Title: 주식가격, Time: 50.83 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,26 +1,27 @@
+import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
         
-        // 1 2 3 2 3 
-        // 4 3 1 1 0
+        Stack<int[]> s = new Stack<>();
+        s.push(new int[]{prices[0], 0});
         
-        // 가격이 떨어졌다? 뒤에 나온 수가 나보다 작을 때
-        // 초가 넘아가면 무조건 1초 증가
-        // 나보다 작다 > 거기서 끝
-        // 나보다 크거나 같다 > 다음
-        int len = prices.length;
-        int[] answer = new int[len];
-        for (int i = 0; i < len; i++){
-            for (int j = i+1; j < len; j++){
-                if (prices[i] <= prices[j])
-                    answer[i]++;
-                else {
-                    answer[i] = j - i;
-                    break;
-                }
+        for (int i = 1; i < prices.length; i++){
+            
+            while(!s.isEmpty() && s.peek()[0] > prices[i]){
+                int[] pre = s.peek();
+                answer[pre[1]] = i - pre[1];
+                s.pop();
             }
+            
+            s.push(new int[]{prices[i], i});
         }
         
+        while (!s.isEmpty()){
+            int[] cur = s.pop();
+            answer[cur[1]] = prices.length - 1 - cur[1];
+        }
+    
         return answer;
     }
 }
