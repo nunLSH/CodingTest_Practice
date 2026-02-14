05d315f7e9e46bffc3dd81979355ee504fd7b8a5
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2026-02-15 00:45:20 +0900

    [Silver IV] Title: 수 찾기, Time: 472 ms, Memory: 48464 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,26 +1,28 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
 
-        HashSet<String> set = new HashSet<>();
+        int N = Integer.parseInt(br.readLine());
+        HashSet<Integer> set = new HashSet<>();
+
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++)
-            set.add(st.nextToken());
+            set.add(Integer.parseInt(st.nextToken()));
 
         int M = Integer.parseInt(br.readLine());
         StringTokenizer st2 = new StringTokenizer(br.readLine());
-        StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < M; i++){
-            if (set.contains(st2.nextToken()))
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            if (set.contains(Integer.parseInt(st2.nextToken())))
                 sb.append(1).append('\n');
-            else 
+            else
                 sb.append(0).append('\n');
         }
 
-        System.out.println(sb.toString());     
+        System.out.print(sb);
     }
 }
