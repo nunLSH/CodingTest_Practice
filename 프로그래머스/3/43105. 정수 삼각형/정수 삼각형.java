debd9a50e41723d1e61bf1cb3bedca56b60acd4a
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2026-02-15 23:44:35 +0900

    [level 3] Title: 정수 삼각형, Time: 9.36 ms, Memory: 63 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,12 +1,32 @@
 class Solution {
-    public int solution(int[][] triangle) {
+    public void dfs(int level, int[][] triangle, int[][] dp){
+        if (level == triangle.length)
+            return;
         
-        for (int i = triangle.length - 2; i >= 0; i--){
-            for (int j = 0; j < triangle[i].length; j++){
-                triangle[i][j] = triangle[i][j] + Math.max(triangle[i+1][j], triangle[i+1][j+1]);
-            }
+        for (int pos = 0; pos < triangle[level].length; pos++){
+            if (pos == 0)
+                dp[level][pos] = triangle[level][pos] + dp[level-1][pos];
+            else
+                dp[level][pos] = triangle[level][pos] 
+                + Math.max(dp[level-1][pos-1], dp[level-1][pos]);
         }
         
-        return triangle[0][0];
+        dfs(level + 1, triangle, dp);
+
+        return;
+    }
+    
+    public int solution(int[][] triangle) {
+        int[][] dp = new int[triangle.length][triangle.length];
+        dp[0][0] = triangle[0][0];
+        
+        dfs(1, triangle, dp);
+        
+        int maxSum = 0;
+        int lastLevel = triangle.length;
+        for (int i = 0; i < triangle[lastLevel - 1].length; i++)
+            maxSum = Math.max(dp[lastLevel - 1][i], maxSum);
+        
+        return maxSum;
     }
 }
