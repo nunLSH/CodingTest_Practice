65f24247716f192d8b435620c3fcd1c3344597ad
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2026-03-11 20:00:05 +0900

    [Gold V] Title: 토마토, Time: 540 ms, Memory: 103456 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -2,67 +2,59 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    public static int[][] tomato;
-    public static int n, m;
-    public static int[] dx = new int[]{-1, 1, 0, 0}; // 왼 오 앞 뒤
-    public static int[] dy = new int[]{0, 0, 1, -1};
-    public static int day = 0;
-    public static int cntTomato = 0;
-    public static int noTomato = 0;
-    
+    static int n, m;
+    static int[][] tomato;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        // 토마토 채우기
         m = Integer.parseInt(st.nextToken());
         n = Integer.parseInt(st.nextToken());
+
         tomato = new int[n][m];
-
         Queue<int[]> q = new ArrayDeque<>();
-        for (int i = 0; i < n; i++){
-            StringTokenizer st2 = new StringTokenizer(br.readLine());
-            for (int j = 0; j < m; j++){
-                tomato[i][j] = Integer.parseInt(st2.nextToken());
-                if (tomato[i][j] == 1){
-                    cntTomato++;
-                    q.offer(new int[]{i, j, 0});
-                } else if (tomato[i][j] == -1)
-                    noTomato++;
-            }
-        }
 
-        // 저장될 때부터 익어있는 상태
-        if (cntTomato == n * m - noTomato){
-            System.out.println(0);
-            return;
-        }
-            
-
-        // BFS
-        while(!q.isEmpty()){
-            int[] cur = q.poll();
-
-            for (int i = 0; i < 4; i++){
-                int nx = cur[0] + dx[i];
-                int ny = cur[1] + dy[i];
-
-                if (inRange(nx, ny) && tomato[nx][ny] == 0){
-                    tomato[nx][ny] = 1;
-                    cntTomato++;
-                    q.offer(new int[]{nx, ny, cur[2] + 1});
-                    day = cur[2] + 1;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                tomato[i][j] = Integer.parseInt(st.nextToken());
+                if (tomato[i][j] == 1) {
+                    q.offer(new int[]{i, j}); // 시작점
                 }
             }
         }
 
-        if (cntTomato == n * m - noTomato)
-            System.out.println(day);
-        else
-            System.out.println(-1);
-    }
+        // BFS
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
 
-    public static boolean inRange(int x, int y){
-        return (0 <= x && x < n && 0 <= y && y < m);
+            for (int d = 0; d < 4; d++) {
+                int nx = cur[0] + dx[d];
+                int ny = cur[1] + dy[d];
+
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+                if (tomato[nx][ny] != 0) continue;
+
+                tomato[nx][ny] = tomato[cur[0]][cur[1]] + 1;
+                q.offer(new int[]{nx, ny});
+            }
+        }
+
+        int result = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (tomato[i][j] == 0) { // 안 익은 토마토 존재
+                    System.out.println(-1);
+                    return;
+                }
+                result = Math.max(result, tomato[i][j]);
+            }
+        }
+
+        System.out.println(result - 1); // 처음 익은 토마토를 1로 시작했기 때문에 -1
     }
 }
