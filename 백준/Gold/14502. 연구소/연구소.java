ada0651563e0df7311c43dab0af8a0aa4a43c1c4
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2026-03-13 18:48:40 +0900

    [Gold IV] Title: 연구소, Time: 1088 ms, Memory: 307652 KB -BaekjoonHub

---
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -0,0 +1,92 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static int[] dx = new int[]{0, 0, -1, 1}; // 상 하 좌 우
+    public static int[] dy = new int[]{-1, 1, 0, 0};
+    public static int[][] lab;
+    public static int n, m;
+    public static int safe = 0;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        // 연구소
+        lab = new int[n][m];
+        for (int i = 0; i < n; i++){
+            StringTokenizer st2 = new StringTokenizer(br.readLine());
+            for(int j = 0; j < m; j++){
+                lab[i][j] = Integer.parseInt(st2.nextToken());
+            }
+        }
+
+        // 벽 세우기 (DFS) > 바이러스 퍼뜨리기 (BFS)
+        dfs(0);
+
+        System.out.println(safe);
+    }
+
+    public static void dfs(int count){
+        // 벽 3개 세움
+        if (count == 3){
+            // 맵 복사
+            int[][] map = new int[n][m];
+            for (int i = 0; i < n; i++)
+                map[i] = lab[i].clone();
+            // 바이러스 퍼뜨리고 안전구역 개수 세기
+            safe = Math.max(bfs(map), safe);
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (lab[i][j] == 0) {
+                    lab[i][j] = 1;     
+                    dfs(count + 1);
+                    lab[i][j] = 0;      
+                }
+            }
+        } 
+    }
+
+    public static int bfs(int[][] map) {
+        Queue<int[]> q = new ArrayDeque<>();
+
+        // 바이러스 큐에 넣기
+        for (int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if (map[i][j] == 2)
+                    q.offer(new int[]{i, j});
+            }
+        }
+
+        while(!q.isEmpty()){
+            int[] cur = q.poll();
+            int x = cur[0];
+            int y = cur[1];
+
+            for (int i = 0; i < 4; i++){
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m && map[nx][ny] == 0){
+                    map[nx][ny] = 2;
+                    q.offer(new int[]{nx, ny});
+                }
+            }
+        }
+
+        // 안전구역 개수 세기
+        int cnt = 0;
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++){
+                if (map[i][j] == 0)
+                    cnt++;
+            }
+        }
+        
+        return cnt;
+    }
+}
