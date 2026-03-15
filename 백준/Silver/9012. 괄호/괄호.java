0eae25380b40bed2e607f95ee696e43b85788f1f
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2026-03-15 22:00:29 +0900

    [Silver IV] Title: 괄호, Time: 104 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++){
+            String s = br.readLine();
+
+            boolean isVPS = true;
+            Stack<String> stack = new Stack<>();
+            for (int j = 0; j < s.length(); j++){
+                if (s.charAt(j) == '(')
+                    stack.push("c");
+                else {
+                    if (stack.isEmpty()){
+                        isVPS = false;
+                        break;
+                    } 
+                    stack.pop();
+                }
+            }
+
+            if (stack.isEmpty() && isVPS)
+                System.out.println("YES");
+            else 
+                System.out.println("NO");
+        }
+    }
+}
