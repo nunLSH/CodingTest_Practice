0ff439dbe8089b750c8bc4fc3005809a9d7eda36
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2026-03-15 22:02:45 +0900

    [Silver IV] Title: 괄호, Time: 104 ms, Memory: 14420 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,29 +1,32 @@
+import java.util.*;
 import java.io.*;
 
 class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++){
             String s = br.readLine();
 
-            int count = 0;
             boolean isVPS = true;
-
-            for (char c : s.toCharArray()) {
-                if (c == '(') count++;
-                else count--;
-
-                if (count < 0) {
-                    isVPS = false;
-                    break;
+            Stack<Character> stack = new Stack<>();
+            for (int j = 0; j < s.length(); j++){
+                if (s.charAt(j) == '(')
+                    stack.push(s.charAt(j));
+                else {
+                    if (stack.isEmpty()){
+                        isVPS = false;
+                        break;
+                    } 
+                    stack.pop();
                 }
             }
 
-            if (count != 0) isVPS = false;
-
-            System.out.println(isVPS ? "YES" : "NO");
+            if (stack.isEmpty() && isVPS)
+                System.out.println("YES");
+            else 
+                System.out.println("NO");
         }
     }
 }
