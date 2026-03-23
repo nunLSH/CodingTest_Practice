51994ceaf77f275eef7062e2a7e6539cb8018342
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2026-03-23 20:42:36 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 168 ms, Memory: 24536 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -1,24 +1,9 @@
 import java.util.*;
 import java.io.*;
 
-class Node implements Comparable<Node>{
-    int to;
-    int cost;
-
-    public Node(int to, int cost){
-        this.to = to;
-        this.cost = cost;
-    }
-
-    @Override
-    public int compareTo(Node o) {
-        return this.cost - o.cost;
-    }
-}
-
 class Main {
     public static final int MAX = 200000;
-    
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -26,28 +11,33 @@ class Main {
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        int[] dist = new int[MAX+1];
-        Arrays.fill(dist, MAX);
-        dist[n] = 1;
-        pq.offer(new Node(n, 0));
+        int[] dist = new int[MAX + 1];
+        Arrays.fill(dist, -1); // -1이면 방문 안 함
+        Deque<Integer> dq = new ArrayDeque<>();
 
-        while(!pq.isEmpty()){
-            Node cur = pq.poll();
+        dist[n] = 0; // 시작점 시간 0초
+        dq.offerFirst(n);
 
-            if (cur.to == k){
-                System.out.println(dist[cur.to] - 1);
+        while (!dq.isEmpty()) {
+            int cur = dq.pollFirst();
+
+            if (cur == k) {
+                System.out.println(dist[cur]);
                 break;
             }
 
-            if (dist[cur.to] < cur.cost) continue;
+            // 0초 이동
+            int teleport = cur * 2;
+            if (teleport <= MAX && dist[teleport] == -1) {
+                dist[teleport] = dist[cur];
+                dq.offerFirst(teleport); // 앞에 넣음 → 우선 처리
+            }
 
-            int[] next = new int[]{cur.to-1, cur.to+1, cur.to*2};
-            int[] time = new int[]{1, 1, 0};
-            for (int i = 0; i < 3; i++){
-                if (next[i] >= 0 && next[i] <= MAX && dist[next[i]] > dist[cur.to] + time[i]) {
-                    dist[next[i]] = dist[cur.to] + time[i];
-                    pq.offer(new Node(next[i], dist[next[i]]));
+            // 1초 이동
+            for (int next : new int[]{cur - 1, cur + 1}) {
+                if (next >= 0 && next <= MAX && dist[next] == -1) {
+                    dist[next] = dist[cur] + 1;
+                    dq.offerLast(next); // 뒤에 넣음 → 나중 처리
                 }
             }
         }
