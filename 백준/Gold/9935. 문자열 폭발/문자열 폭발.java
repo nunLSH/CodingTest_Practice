06e9dd1c5489e75b7f99775006b994a1fb72bb7d
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2026-04-04 22:10:50 +0900

    [Gold IV] Title: 문자열 폭발, Time: 360 ms, Memory: 25368 KB -BaekjoonHub

---
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+  
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+        String bomb = br.readLine();
+        
+        StringBuilder sb = new StringBuilder();   
+        int bombLen = bomb.length();
+        for (char c : s.toCharArray()){
+            sb.append(c);
+
+            if (sb.length() >= bombLen){
+                boolean match = true;
+
+                for (int i = 0; i < bombLen; i++){
+                    if (sb.charAt(sb.length() - bombLen + i) != bomb.charAt(i)){
+                        match = false;
+                        break;
+                    }
+                }
+
+                if (match){
+                    sb.delete(sb.length() - bombLen, sb.length());
+                }
+            }
+        }
+        
+        System.out.println( sb.length() == 0 ? "FRULA" : sb.toString() );
+    }
+}
