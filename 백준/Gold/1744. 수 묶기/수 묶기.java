d65e3636b8ea6c65213245b0f2197f159ba7d7be
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2026-04-20 23:54:47 +0900

    [Gold IV] Title: 수 묶기, Time: 100 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Gold/1744. 수 묶기/수 묶기.java
+++ 백준/Gold/1744. 수 묶기/수 묶기.java
@@ -1,12 +1,46 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        List<Integer> plus = new ArrayList<>();
+        List<Integer> minus = new ArrayList<>();
+        int oneCount = 0;
+
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            if (num > 1) plus.add(num);
+            else if (num == 1) oneCount++;
+            else minus.add(num); 
+        }
+
+        Collections.sort(plus); 
+        Collections.sort(minus);
+        long answer = 0;
+
+        // 1은 그냥 더하기
+        answer += oneCount;
+
+        // 양수 처리 (큰 수끼리 곱하기)
+        for (int i = plus.size() - 1; i >= 0; i -= 2) {
+            if (i - 1 >= 0)
+                answer += (long) plus.get(i) * plus.get(i - 1);
+            else
+                answer += plus.get(i);
+        }
+
+        // 음수 + 0 처리 (절댓값 큰 음수끼리)
+        for (int i = 0; i < minus.size(); i += 2) {
+            if (i + 1 < minus.size())
+                answer += (long) minus.get(i) * minus.get(i + 1);
+            else
+                answer += minus.get(i);
+        }
+
+        System.out.println(answer);
+    }
+}
