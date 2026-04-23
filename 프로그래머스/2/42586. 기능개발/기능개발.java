c6f5b0693e3c11135bb6870248b38c794d477073
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2026-04-23 22:13:12 +0900

    [level 2] Title: 기능개발, Time: 0.40 ms, Memory: 86.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,27 +2,40 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        // 남은 작업 시간 계산
-        int len = progresses.length;
-        int[] complete = new int[len];
-        for (int i = 0; i < len; i++)
-            complete[i] = (int) Math.ceil((double)(100 - progresses[i]) / speeds[i]);
 
-        // FIFO >> 배포되는 기능 계산
-        ArrayList<Integer> answer = new ArrayList<>();
-        int max = complete[0];
-        int cnt = 0;
-        for (int i = 0; i < len; i++){
-            if (max >= complete[i])
-                cnt++;
-            else {
-                answer.add(cnt);
-                cnt = 1;
-                max = complete[i];
-            }
+        Queue<Integer> q = new ArrayDeque<>();
+        for (int i = 0; i < progresses.length; i++){
+            // 남은 작업량 계산
+            int remain = 100 - progresses[i]; 
+            
+            // 배포까지 걸리는 시간 계산
+            int days = remain % speeds[i] == 0 ? remain / speeds[i] : remain / speeds[i] + 1;
+            
+            // 큐에 순서대로 넣기
+            q.offer(days);
         }
-        answer.add(cnt);
         
-        return answer.stream().mapToInt(Integer::intValue).toArray();
+        List<Integer> ll = new ArrayList<>();
+        // 배포되는 기능 개수 세기
+        while (!q.isEmpty()){
+            int cnt = 1;
+            int deploy = q.poll();
+            
+            // 먼저 배포되는 것보다 작업 일수가 적은 것들 함께 배포
+            while (!q.isEmpty() && deploy >= q.peek()){
+                q.poll();
+                cnt++;
+            }
+            
+            ll.add(cnt);
+        }
+        
+        System.out.println(ll.size());
+        
+        int[] answer = new int[ll.size()];
+        for (int i = 0; i < ll.size(); i++)
+            answer[i] = ll.get(i);
+        
+        return answer;
     }
 }
