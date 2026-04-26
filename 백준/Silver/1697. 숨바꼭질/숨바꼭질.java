4ed94acab56249a3d76684191316052162358b06
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2026-04-26 22:27:35 +0900

    [Silver I] Title: 숨바꼭질, Time: 168 ms, Memory: 26164 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -2,32 +2,32 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    public static final int max = 200000;
+    public static final int MAX = 100000 * 2;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
- 
-        // BFS
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] dist = new int[MAX+1];
         Queue<Integer> q = new ArrayDeque<>();
-        int[] dist = new int[max + 1];
         
-        q.offer(n);
-        dist[n] = 1;
+        q.offer(N);
+        dist[N] = 1;
         
-        while (!q.isEmpty()) {
+        while(!q.isEmpty()){
             int pos = q.poll();
-        
-            if (pos == k) {
-                System.out.println(dist[pos]-1);
-                return;
+
+            if (pos == K){
+                System.out.println(dist[K] - 1);
+                break;
             }
-        
-            for (int next : new int[]{pos*2, pos+1, pos-1}) {
-                if (next >= 0 && next <= max && dist[next] == 0) {
-                    dist[next] = dist[pos] + 1;
-                    q.offer(next);
+
+            int[] next = new int[]{pos+1, pos-1, pos*2};
+            for (int n : next){
+                if (0 <= n && n <= MAX && dist[n] == 0){
+                    dist[n] = dist[pos] + 1;
+                    q.offer(n);
                 }
             }
         }
