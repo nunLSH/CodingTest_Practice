c5129aba10b92919bb492ba44e2c550dadaefea5
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2026-05-20 23:15:52 +0900

    [level 2] Title: 의상, Time: 0.10 ms, Memory: 58.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,14 +1,18 @@
 import java.util.*;
+
 class Solution {
     public int solution(String[][] clothes) {
-        // 각 종류 별 몇 개인지
-        Map<String, Integer> map = new HashMap<>();
-        for (String[] s : clothes)
-            map.put(s[1], map.getOrDefault(s[1], 0) + 1);
-        
         int answer = 1;
-        for (int j : map.values())
-            answer *= (j+1);
+        
+        Map<String, Integer> map = new HashMap<>();
+        for (String[] wear : clothes){
+            map.put(wear[1], map.getOrDefault(wear[1], 0) + 1);
+        }
+        
+        for (String s : map.keySet()){
+            int num = map.get(s);
+            answer *= (num + 1);
+        }
         
         return answer - 1;
     }
