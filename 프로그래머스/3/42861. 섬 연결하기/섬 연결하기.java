a1bed1444448a340b34321078b78ad914eebc6d5
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2026-05-23 22:29:15 +0900

    [level 3] Title: 섬 연결하기, Time: 1.69 ms, Memory: 59.6 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,62 +1,68 @@
 import java.util.*;
 
-class Edge {
-    int from;
-    int to;
-    int cost;
-    
-    public Edge(int from, int to, int cost){
-        this.from = from;
-        this.to = to;
-        this.cost = cost;
-    }
-}
-
 class Solution {
-    public List<Edge> edges;
-    public int[] parent;
-    
+
+    static class Edge {
+        int from;
+        int to;
+        int cost;
+
+        Edge(int from, int to, int cost) {
+            this.from = from;
+            this.to = to;
+            this.cost = cost;
+        }
+    }
+
+    int[] parent;
+
     public int solution(int n, int[][] costs) {
-        int answer = 0;
-        
-        edges = new ArrayList<>();
-        parent = new int[n];
-        
-        for (int[] cost: costs)
+        List<Edge> edges = new ArrayList<>();
+
+        for (int[] cost : costs) {
             edges.add(new Edge(cost[0], cost[1], cost[2]));
-        
+        }
+
         edges.sort(Comparator.comparingInt(e -> e.cost));
-        
-        int count = 0;
-        for (int i = 0; i < n; i++)
+
+        parent = new int[n];
+
+        for (int i = 0; i < n; i++) {
             parent[i] = i;
-        
-        for (Edge edge : edges){
-            if (find(edge.from) != find(edge.to)){
+        }
+
+        int answer = 0;
+        int count = 0;
+
+        for (Edge edge : edges) {
+            if (find(edge.from) != find(edge.to)) {
                 union(edge.from, edge.to);
                 answer += edge.cost;
                 count++;
+
+                if (count == n - 1) {
+                    break;
+                }
             }
-            
-            if (count == n - 1)
-                break;
         }
-        
+
         return answer;
     }
-    
-    public void union(int a, int b){
-        a = find(a);
-        b = find(b);
-        
-        if (a != b)
-            parent[b] = a;        
-    }
-    
-    public int find(int x){
-        if (parent[x] == x)
+
+    private int find(int x) {
+        if (parent[x] == x) {
             return x;
-        
-        return find(parent[x]);
+        }
+
+        return parent[x] = find(parent[x]);
+    }
+
+    private void union(int a, int b) {
+        int rootA = find(a);
+        int rootB = find(b);
+
+        if (rootA != rootB) {
+            parent[rootB] = rootA;
+        }
     }
 }
